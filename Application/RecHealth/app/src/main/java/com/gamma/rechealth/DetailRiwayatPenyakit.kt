4d220ee4516e41b312a85c75d9f5374a8b0043f2
rifkiystark <ananda.rifkiy32@gmail.com>
2020-11-24 01:27:18 +0700

    add detail riwayat penyakit

---
+++ Application/RecHealth/app/src/main/java/com/gamma/rechealth/DetailRiwayatPenyakit.kt
@@ -0,0 +1,12 @@
+package com.gamma.rechealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailRiwayatPenyakit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_riwayat_penyakit)
+
+    }
+}
