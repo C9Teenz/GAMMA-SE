ef479f0fc10166fff02338df07d4807b380d9eb5
ronny12062000 <56969000+ronny12062000@users.noreply.github.com>
2020-11-23 22:37:12 +0700

    T3,T4,T7,T13,T16,T18,T20

---
+++ Application/RecHealth/app/src/main/java/com/gamma/rechealth/FormDaftarCheckUp.kt
@@ -0,0 +1,11 @@
+package com.gamma.rechealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormDaftarCheckUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_daftar_check_up)
+    }
+}
