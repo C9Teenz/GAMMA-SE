2e73859b2fbb64aa0f8aa84d0adc9cbc4bf1bf4c
rifkiystark <ananda.rifkiy32@gmail.com>
2020-11-26 11:32:07 +0700

    add onclick @HalamanAwal

--- Application/RecHealth/app/src/main/java/com/gamma/rechealth/HalamanAwal.kt
+++ Application/RecHealth/app/src/main/java/com/gamma/rechealth/HalamanAwal.kt
@@ -1,11 +1,23 @@
 package com.gamma.rechealth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_halaman_awal.*
 
 class HalamanAwal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_halaman_awal)
+
+        btnDaftar.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnMasuk.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
